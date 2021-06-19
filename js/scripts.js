14880530c1d19b03f3d016a60d4d9e4cbee948a1
Francis Kinyae <fmkinyae@gmail.com>
2021-06-19 23:14:05 +0300

    add jquery  hover function

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    
+    $("div#design-text").click(function(){        
+        $("div#design-text").toggle();
+    });
+    $("div#design-text").click(function(){        
+        $("div#design-text").toggle();
+    });
+    $("div#design-text").click(function(){        
+        $("div#design-text").toggle();
+    });
+
+
+
+    //Hovering Images On Portfolio Section.
+    $("#black").mouseover(function(){$("#overlay").show();
+    })
+    .mouseout(function(){$("#overlay").hide();
+    });
+    $("#ontario").mouseover(function(){$("#overlay1").show();
+    })
+    .mouseout(function(){$("#overlay1").hide();
+    });
+    $("#pyramids").mouseover(function(){$("#overlay2").show();
+    })
+    .mouseout(function(){$("#overlay2").hide();
+    });
+    $("#jim").mouseover(function(){$("#overlay3").show();
+    })
+    .mouseout(function(){$("#overlay3").hide();
+    });
+    $("#nice").mouseover(function(){$("#overlay4").show();
+    })
+    .mouseout(function(){$("#overlay4").hide();
+    });
+    $("#calculator").mouseover(function(){$("#overlay5").show();
+    })
+    .mouseout(function(){$("#overlay5").hide();
+    });
+    $("#burned").mouseover(function(){$("#overlay6").show();
+    })
+    .mouseout(function(){$("#overlay6").hide();
+    });
+    $("#jim").mouseover(function(){$("#overlay3").show();
+    })
+    .mouseout(function(){$("#overlay3").hide();
+    });
+    $("#giraffe").mouseover(function(){$("#overlay7").show();
+    })
+    .mouseout(function(){$("#overlay7").hide();
+    });
+
+  });
